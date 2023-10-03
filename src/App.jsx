168bb761f32a10c89a25a726a14fafbd735e063c
David <doladepo128@gmail.com>
2023-10-03 22:37:48 +0100

    added background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Topimage from './assets/bg-pattern-top.svg';
+import Bottomimage from './assets/bg-pattern-bottom.svg';
+import card from './assets/bg-pattern-card.svg';
+import person from './assets/image-victor.jpg';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <article>
-          <img src="profile"  />
+          <img src={Topimage} alt="top background" />
+        
+        <article className='card'>
+
+          <img src={person} alt='picture of a man'  />
           <h1>Victor Crest 26</h1>
           <p>London</p>
 
@@ -33,6 +40,7 @@ function App() {
           </footer>
         </article>
         
+       <img src={Bottomimage} alt="bottom background" />
       </div>
       
      
@@ -41,4 +49,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
